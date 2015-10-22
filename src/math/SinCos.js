a5c1b9b19ab08e23777a5c37e8c96b4b7db8267e
Richard Davey <rdavey@gmail.com>
2015-10-22 02:41:01 +0100

    RandomDataGenerator done along with lots of other Math functions.

--- src/math/SinCos.js
+++ src/math/SinCos.js
@@ -1,33 +1,21 @@
-/**
-* Generate a sine and cosine table simultaneously and extremely quickly.
-* The parameters allow you to specify the length, amplitude and frequency of the wave.
-* This generator is fast enough to be used in real-time.
-* Code based on research by Franky of scene.at
-*
-* @method Phaser.Math#sinCosGenerator
-* @param {number} length - The length of the wave
-* @param {number} sinAmplitude - The amplitude to apply to the sine table (default 1.0) if you need values between say -+ 125 then give 125 as the value
-* @param {number} cosAmplitude - The amplitude to apply to the cosine table (default 1.0) if you need values between say -+ 125 then give 125 as the value
-* @param {number} frequency  - The frequency of the sine and cosine table data
-* @return {{sin:number[], cos:number[]}} Returns the table data.
-*/
+
 export default function (length, sinAmp = 1.0, cosAmp = 1.0, frequency = 1.0) {
 
     frequency *= Math.PI / length;
 
-    let cosTable = [];
-    let sinTable = [];
+    let cos = [];
+    let sin = [];
 
     for (let c = 0; c < length; c++) {
 
         cosAmp -= sinAmp * frequency;
         sinAmp += cosAmp * frequency;
 
-        cosTable[c] = cosAmp;
-        sinTable[c] = sinAmp;
+        cos[c] = cosAmp;
+        sin[c] = sinAmp;
 
     }
 
-    // return { sinTable, cosTable, length };
+    return { sin, cos, length };
 
 }
