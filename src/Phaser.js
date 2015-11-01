bfaa9394552f3564c4b44e47265aa98e6e90178e
Richard Davey <rdavey@gmail.com>
2015-11-01 11:22:24 +0000

    Version.js will be automatically generated by the build process. Phaser.js is the start of our core object.

---
+++ src/Phaser.js
@@ -0,0 +1,7 @@
+var Phaser = {
+
+    TWOPI: Math.PI * 2,
+    HALFPI: Math.PI * 0.5,
+    EPSILON: 1.0e-6
+
+};
---
+++ src/Version.js
@@ -0,0 +1,5 @@
+Phaser.Version = {
+    'version': '1.0',
+    'build': 1,
+    'date': '31st October 2015'
+};
