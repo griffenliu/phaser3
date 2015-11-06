458e3852f574291ba3a51834d35f0215d21c478e
photonstorm <rich@photonstorm.com>
2015-11-06 17:47:05 +0000

    This makes much more sense as a function that returns an object, rather than class.

--- src/webgl/ContextOptions.js
+++ src/webgl/ContextOptions.js
@@ -1,13 +1,12 @@
-export default class WebGLContextOptions {
 
-    constructor (alpha = false, antialias = true, premultipliedAlpha = false, stencil = false, preserveDrawingBuffer = false) {
+export default function WebGLContextOptions ({ alpha = false, antialias = true, premultipliedAlpha = false, stencil = false, preserveDrawingBuffer = false } = {}) {
 
-        this.alpha = alpha;
-        this.antialias = antialias;
-        this.premultipliedAlpha = premultipliedAlpha;
-        this.stencil = stencil;
-        this.preserveDrawingBuffer = preserveDrawingBuffer;
-
+    return {
+        alpha: alpha,
+        antialias: antialias,
+        premultipliedAlpha: premultipliedAlpha,
+        stencil: stencil,
+        preserveDrawingBuffer: preserveDrawingBuffer
     }
 
 }
