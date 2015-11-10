3b827727784d11d53f4ae43d50fa7a0b6f325518
Richard Davey <rdavey@gmail.com>
2015-11-10 02:04:41 +0000

    Finished ortho function.

--- src/math/matrix/mat43/Orthonormalize.js
+++ src/math/matrix/mat43/Orthonormalize.js
@@ -1,4 +1,8 @@
-import Vec3Math from 'math/vector/vec3/Vec3Math.js';
+import Normalize from 'math/vector/vec3/Normalize.js';
+import Length from 'math/vector/vec3/Length.js';
+import Dot from 'math/vector/vec3/Dot.js';
+import Cross from 'math/vector/vec3/Cross.js';
+
 import Right from 'math/matrix/mat43/Right.js';
 import Up from 'math/matrix/mat43/Up.js';
 import At from 'math/matrix/mat43/At.js';
@@ -6,39 +10,33 @@ import Pos from 'math/matrix/mat43/Pos.js';
 
 let mat = Float32Array;
 
-//  TODO - Needs finishing (once Vec3 is split up)
-
 export default function (m, dst = new mat(12)) {
 
-    // var normalize = VMath.v3Normalize;
-    // var length    = VMath.v3Length;
-    // var dot       = VMath.v3Dot;
-    // var cross     = VMath.v3Cross;
-    // var abs       = Math.abs;
+    const right = Right(m);
+    const up = Up(m);
+    const at = At(m);
+    const pos = Pos(m);
 
-    // var right = VMath.m43Right(m);
-    // var up    = VMath.m43Up(m);
-    // var at    = VMath.m43At(m);
-    // var pos   = VMath.m43Pos(m);
+    const innerX = Length(right);
+    const innerY = Length(up);
+    const innerZ = Length(at);
 
-    var innerX = length(right);
-    var innerY = length(up);
-    var innerZ = length(at);
+    Normalize(right, right);
+    Normalize(up, up);
+    Normalize(at, at);
 
-    normalize(right, right);
-    normalize(up, up);
-    normalize(at, at);
+    let vpU, vpV, vpW;
 
-    var vpU, vpV, vpW;
-    if (innerX > 0.0)
+    if (innerX > 0)
     {
-        if (innerY > 0.0)
+        if (innerY > 0)
         {
-            if (innerZ > 0.0)
+            if (innerZ > 0)
             {
-                var outerX = abs(dot(up, at));
-                var outerY = abs(dot(at, right));
-                var outerZ = abs(dot(right, up));
+                let outerX = Math.abs(Dot(up, at));
+                let outerY = Math.abs(Dot(at, right));
+                let outerZ = Math.abs(Dot(right, up));
+
                 if (outerX < outerY)
                 {
                     if (outerX < outerZ)
@@ -91,21 +89,24 @@ export default function (m, dst = new mat(12)) {
         vpW = right;
     }
 
-    cross(vpU, vpV, vpW);
-    normalize(vpW, vpW);
+    Cross(vpU, vpV, vpW);
+    Normalize(vpW, vpW);
 
-    cross(vpW, vpU, vpV);
-    normalize(vpV, vpV);
+    Cross(vpW, vpU, vpV);
+    Normalize(vpV, vpV);
 
     dst[0] = right[0];
     dst[1] = right[1];
     dst[2] = right[2];
+
     dst[3] = up[0];
     dst[4] = up[1];
     dst[5] = up[2];
+
     dst[6] = at[0];
     dst[7] = at[1];
     dst[8] = at[2];
+
     dst[9] = pos[0];
     dst[10] = pos[1];
     dst[11] = pos[2];
